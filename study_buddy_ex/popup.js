b4278a4d9c03970e25173bd6a746201ebaa7c848
Spencer Fulgham <spencerman@comcast.net>
2022-01-15 16:16:41 -0800

    finished timer cycle and bgpage

--- study_buddy_ex/popup.js
+++ study_buddy_ex/popup.js
@@ -1,19 +1,28 @@
-is_working = false;
-work_tim = 30;
+var bgpage = chrome.extension.getBackgroundPage();
+
+var started = false;
+is_working = true;
+work_tim = 5;
 break_min = 10;
+
 vibe = null;
 
-// document.getElementById("bad").addEventListener("click", vibe_check);
-window.onload = function () {
-  tick();
 
-  // vibe check buttons
-  document.getElementById("bad").onclick    = function(){vibe_check(1)};
-  document.getElementById("poor").onclick   = function(){vibe_check(2)};
-  document.getElementById("decent").onclick = function(){vibe_check(3)};
-  document.getElementById("okay").onclick   = function(){vibe_check(4)};
-  document.getElementById("great").onclick  = function(){vibe_check(5)};
-}
+// document.getElementById("bad").addEventListener("click", vibe_check);
+document.addEventListener("DOMContentLoaded", function() {
+  // var port = chrome.runtime.connect();
+
+  // document.getElementById("bad").onclick    = function(){vibe_check(1)};
+  // document.getElementById("poor").onclick   = function(){vibe_check(2)};
+  // document.getElementById("decent").onclick = function(){vibe_check(3)};
+  // document.getElementById("okay").onclick   = function(){vibe_check(4)};
+  // document.getElementById("great").onclick  = function(){vibe_check(5)};
+
+  const ev = document.getElementById("time_start");
+  ev.addEventListener("click", init_timers, false);
+
+  refreshDisplay();
+});
 
 function vibe_check(v){
   // get form from html
@@ -25,47 +34,47 @@ function vibe_check(v){
   console.log(vibe)
 }
 
-function tick(){
+function init_timers(){
+  bgpage.set_work_duration(20)
+  bgpage.set_break_duration(10)
+  start_timer();
+}
+
+function start_timer(){
+  bgpage.set_alarm(is_working);
+  // if(is_working){ bgpage.set_alarm(bgpage.work_duration  * 1000);}
+  // else          { bgpage.set_alarm(bgpage.break_duration * 1000);}
+  refreshDisplay();
+}
+
+// function hide(section){
+//     document.getElementById(section).style.display = "none";
+// }
+
+function refresh_timer(){
+  seconds_left = bgpage.user_timer_seconds
+
+  var min = Math.floor(seconds_left/60);
+  var sec = seconds_left - (min*60);
+  if (sec < 10) { sec = "0" + sec; }
+  var msg = min.toString()+":"+sec;
+
   var timeDisplay = document.getElementById("time");
-  var min=Math.floor(work_tim/60);
-  var sec=work_tim-(min*60);
-  if (sec < 10) {
-    sec="0"+sec;
-  }
-  var message=min.toString()+":"+sec;
-  timeDisplay.innerHTML=message;
-  setTimeout(function() {
-    if(work_tim != 0){
-        work_tim--;
-        tick();
-    }
-    else{
-      alert("Done");
-      // clearInterval(intervalHandle);
-      resetPage();
-    }
-  }, 1000);
+  timeDisplay.innerHTML = msg;
 
+  refreshDisplayTimeout = setTimeout(refreshDisplay, 1000);
 }
 
+function refreshDisplay(){
+  refresh_timer();
+
+  // if(started){
+    // if(bgpage.user_timer_seconds >= 0){ refresh_timer(); }
+    // else{ refreshDisplay }
+  // }
+  // else{
+  //   started = true;
+  // }
 
 
-function timer_handler(){
-  // var timerInstance = new easytimer.Timer();
-  // take form inputs and store them into cache varibles
-  // work_tim = form.work
-  // break_min = form.break 
-}
-
-function work_timer(){
-  // start visible timer based off of user-set work timer saved in cache
-  // update the timer in real time
-  // when the timer ends, call vibe_check
-  // when vibe_check ends, start break_timer 
-}
-
-function break_timer(){
-  // start visible timer based off of user-set break timer saved in cache
-  // update the timer in real time
-  // when timer ends, call work timer
 }
